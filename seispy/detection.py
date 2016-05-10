48cab0f7cee811cc434096799ae0c474040413c3
Lijun Zhu <gatechzhu@gmail.com>
2016-05-10 15:42:20 -0400

    Convert eps() and cm() to Numba version

--- seispy/detection.py
+++ seispy/detection.py
@@ -72,24 +72,7 @@ def er(trace, Ne):
     Window Size(ne): 2–3 times the dominant period of the signal
     '''
 
-    # Input check
-    # if type(trace).__module__ != np.__name__:
-    #     raise TypeError("data must be a numpy array")
-    # if len(trace.shape) != 1:
-    #     raise ValueError('trace is a one-dimensional array')
-    # if not isinstance(ne, (int, float)):
-    #     raise TypeError("ne must be a number")
-    # if ne <= 0:
-    #     raise ValueError("ne must be a possitive number")
-    # if ne >= len(trace):
-    #     raise ValueError("ne must be less than length of trace")
-
-    Nsp = len(trace)    # number of sample points in trace
-    # Extend trace(both head and tail)
-    # trace_ext = np.hstack((trace,
-    #                        np.mean(trace[-3:-1]) * np.ones(ne - 1),
-    #                        np.mean(trace[:2]) * np.ones(ne - 1)))
-
+    Nsp = len(trace)
     initial_value = (trace[0] + trace[1]) / 2
     end_value = (trace[-2] + trace[-1]) / 2
     # Energy ratio
@@ -112,77 +95,91 @@ def er(trace, Ne):
     return ratio
 
 
-def rolling_window(a, window, step=1):
-    '''Rolling window of a
-
-    Keyword argument:
-    a:  Input 1D array
-    window: Window length
-    step:  steps in sample point
-
-    Return:
-    rolling windowed data
-
-    '''
-    shape = a.shape[:-1] + (int((a.shape[-1] - window) / step) + 1, window)
-    strides = (a.strides[-1] * step,) + (a.strides[-1],)
-    return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
-
-
-def eps(trace, ne):
+@numba.jit(nopython=True)
+def eps(trace, Ne):
     ''' Edge-preserving smoothing
 
     Keyword argument:
-    trace:  Input data
-    ne:     Window length
+    trace:  input data
+    Ne:     window length
 
     Returns:
     trace_eps:   Edge preserved smoothed trace
+
+    Parameter selection:
+    Window length(Ne): times the dominant period of the signal
 
     Reference:
     Luo, Y.,M.Marhoon, S. A. Dossary, andM. Alfaraj, 2002, Edge-preserving smoothing and applications: The Leading Edge, 21, 136–158.
     '''
 
     Nsp = len(trace)
-
-    # Extend trace
-    trace_ext = np.hstack((trace,
-                           np.mean(trace[-3:-1]) * np.ones(ne - 1),
-                           np.mean(trace[:2]) * np.ones(ne - 1)))
+    initial_value = (trace[0] + trace[1]) / 2
+    end_value = (trace[-2] + trace[-1]) / 2
 
     # Initialize output array
-    trace_eps = np.zeros(trace.shape)
+    trace_eps = np.zeros(Nsp)
     for nsp in range(Nsp):
-        windows = rolling_window(trace_ext[range(nsp - ne + 1, nsp + ne)], ne)
-        selected_window_idx = np.argmin(np.std(windows, axis=1))
-        trace_eps[nsp] = np.mean(windows[selected_window_idx])
+        for ne in range(Ne):
+            window = np.zeros(Ne)
+            nc = 0
+            for nw in range(nsp - Ne + 1 + ne, nsp + ne):
+                if nw < 0:
+                    window[nc] = initial_value
+                elif nw >= Nsp:
+                    window[nc] = end_value
+                else:
+                    window[nc] = trace[nw]
+                nc += 1
+            std = window.std()
+            if ne == 0:
+                std_min = std
+                mean_target = window.mean()
+            elif std < std_min:
+                std_min = std
+                mean_target = window.mean()
+        trace_eps[nsp] = mean_target
 
     return trace_eps
 
 
-def cm(trace, nl, ne, beta=0.2, mcm=True):
-    ''' Coppen's method and modified Coppen's method
+@numba.jit(nopython=True)
+def cm(trace, Nl, beta=0.2):
+    ''' Coppens' method
+
+    Keyword argument:
+    trace:  input trace
+    Nl:     leading window length
+    beta:   demoninator factor
+
+    Returns:
+    ratio:  Coppens' method ratio
+
+    Parameter selection:
+    Leading window length(Nl): 1 time the dominant period of the signal
+
+    Reference:
+    Coppens, F. "FIRST ARRIVAL PICKING ON COMMON‐OFFSET TRACE COLLECTIONS FOR AUTOMATIC ESTIMATION OF STATIC CORRECTIONS*." Geophysical Prospecting 33.8 (1985): 1212-1231.
 
     '''
 
     Nsp = len(trace)
 
-    # EPS
-    if mcm:
-        trace_eps = eps(trace, ne)
-    else:
-        trace_eps = trace
-
-    # Extend trace
-    trace_ext = np.hstack((trace_eps, np.zeros(nl - 1)))
-
     # Coppen's method
-    mcm = np.zeros(trace.shape)
+    ratio = np.zeros(Nsp)
     for nsp in range(Nsp):
-        mcm[nsp] = np.sum(trace_ext[range(nsp - nl + 1, nsp + 1)]**2) /\
-            (np.sum(trace_ext[range(nsp + 1)]**2) + beta)
+        E1 = 0
+        for nl in range(nsp - Nl + 1, nsp + 1):
+            if nl >= 0:
+                E1 += trace[nl]**2
+        E2 = 0
+        for nl in range(nsp + 1):
+            E2 += trace[nl]**2
+        ratio[nsp] = E1 / (E2 + beta)
+        # mcm[nsp] = np.sum(trace_ext[range(nsp - nl + 1, nsp + 1)]**2) /\
+        #     (np.sum(trace_ext[range(nsp + 1)]**2) + beta)
 
-    return mcm
+    return ratio
 
 
 def em():
