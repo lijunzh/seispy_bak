f0905cecbdd245fc497bdb441a605c7b95407a3d
Lijun Zhu <gatechzhu@gmail.com>
2016-11-13 11:41:18 -0500

    Add package content to top-level __init__.py

--- seispy/__init__.py
+++ seispy/__init__.py
@@ -1,5 +1,13 @@
 # -*- coding: utf-8 -*-
+''' Lijun Zhu's personal seismic processing Python toolbox'''
 
 __author__ = """Lijun Zhu"""
 __email__ = 'gatechzhu@gmail.com'
 __version__ = '0.1.0'
+
+
+# Import package content
+from . import core
+from . import plot
+from . import detection
+from . import io
